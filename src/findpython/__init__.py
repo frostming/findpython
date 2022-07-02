8d22ece9d59d325c473a7f204042ed5af4c2b85e
Frost Ming <mianghong@gmail.com>
2022-07-02 16:05:40 +0800

    Bump version to 0.2.0

--- src/findpython/__init__.py
+++ src/findpython/__init__.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 from findpython.finder import Finder
 from findpython.python import PythonVersion
 
-__version__ = "0.1.6"
+__version__ = "0.2.0"
 
 
 def find(*args, **kwargs) -> PythonVersion | None:
