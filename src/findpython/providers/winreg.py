aafa77fd8ee1055ccf5c4bd316119400e44a4976
Frost Ming <me@frostming.com>
2024-04-18 19:08:05 +0800

    fix name
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/findpython/providers/winreg.py
+++ src/findpython/providers/winreg.py
@@ -36,15 +36,15 @@ class WinregProvider(BaseProvider):
                 continue
             if path.exists():
                 py_version = getattr(version.info, "version", None)
-                version = None
+                parse_version: Version | None = None
                 if py_version:
                     try:
-                        version = Version(py_version)
+                        parse_version = Version(py_version)
                     except ValueError:
                         pass
                 py_ver = self.version_maker(
                     path,
-                    version,
+                    parse_version,
                     getattr(version.info, "sys_architecture", SYS_ARCHITECTURE),
                     path,
                 )
